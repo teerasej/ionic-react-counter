61155f6aa46b042e4dcadf2f3a14c18e180c6229
Teerasej <teerasej@gmail.com>
2020-01-28 23:11:12 +0700

    prepare start

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter, IonButton } from '@ionic/react';
 import React from 'react';
 
 const Home: React.FC = () => {
@@ -6,19 +6,19 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle>Nextflow Counter (React)</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        <h1 style={{
+          textAlign: 'center',
+          fontSize: '100px'
+        }}>0</h1>
       </IonContent>
+      <IonFooter className="ion-padding">
+          <IonButton expand="block">Increase</IonButton>
+          <IonButton expand="block">Decrease</IonButton>
+      </IonFooter>
     </IonPage>
   );
 };
